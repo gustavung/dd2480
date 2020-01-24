46f66441ab20574bf36660680626ca9e9c373c10
Fabian Waxin <54106108+Boenan@users.noreply.github.com>
2020-01-24 09:49:20 +0100

    Added new feature Launch Interceptor Condition (LIC) number 4. Create… (#25)
    * Added new feature Launch Interceptor Condition (LIC) number 4. Created unittest file with test cases for LIC 4. Changed values for Q_PTS and QUADS in global.json
    
    * Changed the test file name and class name

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import json
 
 f = open("global.json")
-
 inp = json.load(f)
 
 NUMPOINTS = inp["NUMPOINTS"]
 POINTS = inp["POINTS"]
 PARAMETERS_T = inp["PARAMETERS_T"]
+Q_PTS = PARAMETERS_T["Q_PTS"]
+QUADS = PARAMETERS_T["QUADS"]
 LCM = inp["LCM"]
 PUV = inp["PUV"]
-
-print(NUMPOINTS, POINTS, PARAMETERS_T, LCM, PUV)
 
 ###################### Main entrypoint ######################
 
@@ -24,7 +23,7 @@ def generate_LIC():
     CMV[1]  =    False#LIC1()
     CMV[2]  =    False#LIC2()
     CMV[3]  =    False#LIC3()
-    CMV[4]  =    False#LIC4()
+    CMV[4]  =    LIC4()
     CMV[5]  =    False#LIC5()
     CMV[6]  =    False#LIC6()
     CMV[7]  =    False#LIC7()
@@ -36,6 +35,39 @@ def generate_LIC():
     CMV[13] =    False#LIC13()
     CMV[14] =    False#LIC14()
     return CMV
+
+
+"""
+This function creates Launch Interceptor Condition (LIC) number 4.
+Returns true if requirements are met.
+The requirements for LIC 4:
+
+There exist at least one set of Q_PTS consecutive data points that lie in more than QUADS quadrants.
+
+"""
+def LIC4():
+    quads_check = [False for _ in range(0, 4)]
+    c_list = []
+    if 2 <= Q_PTS <= NUMPOINTS and 1 <= QUADS <= 3:
+        for i in range(0, NUMPOINTS - Q_PTS + 1):
+            c_list.append(POINTS[i:i + Q_PTS])
+
+        for i in range(0, len(c_list)):
+            for j in range(0, Q_PTS):
+                if c_list[i][j][0] >= 0 and c_list[i][j][1] >= 0:
+                    quads_check[0] = True
+                elif c_list[i][j][0] <= -1 and c_list[i][j][1] >= 0:
+                    quads_check[1] = True
+                elif c_list[i][j][0] <= 0 and c_list[i][j][1] <= -1:
+                    quads_check[2] = True
+                elif c_list[i][j][0] >= 1 and c_list[i][j][1] <= -1:
+                    quads_check[3] = True
+
+            if len([i for i in range(0, len(quads_check)) if quads_check[i] is True]) > QUADS:
+                return True
+            else:
+                quads_check = [False for _ in range(0, 4)]
+    return False
 
 """Return a tuple of the launch decision, CMV, PUM and FUV
 
@@ -68,4 +100,6 @@ def decide():
 
     return (LAUNCH, CMV, PUM, FUV)
 
-decide()
+
+if __name__ == "__name__":
+    decide()
